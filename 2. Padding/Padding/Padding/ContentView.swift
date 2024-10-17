e3e65d9f8dcdb68cd6e9227da9b6f61a1fe571b6
Venkat <prabhuvenkat46@gmail.com>
2024-10-17 15:50:25 +0530

    Setup Padding all over object

--- 2. Padding/Padding/Padding/ContentView.swift
+++ 2. Padding/Padding/Padding/ContentView.swift
@@ -11,26 +11,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text("Padding")
-        
-        // padding round the object
-            .padding()
+        Text("Hello World..!")
+            .padding() // Adds default padding on all sides
+            .background(Color.cyan)
         
         /*
-        // Padding to the leading Edge
-            .padding(.leading, 20)
+        Text("Custom Padding")
+            .padding(20) // Adds 20 points of padding on all sides
+            .background(Color.green)
         
-        // Padding to the trailing Edge
-            .padding(.trailing, 20)
+        Text("Horizontal Padding")
+            .padding(.horizontal, 30) // Adds 30 points of padding on left and right
+            .background(Color.blue)
         
-        // Padding to the top Edge
-            .padding(.top, 20)
-        
-        // Padding to the bottom Edge
-            .padding(.bottom, 20)
+        Text("Different Padding")
+            .padding(.top, 10) // Adds padding only at the top
+            .padding(.bottom, 20) // Adds padding only at the bottom
+            .padding(.horizontal, 40) // Adds padding to the left and right
+            .background(Color.red)
          */
-        
-            .background(.cyan)
     }
 }
 
