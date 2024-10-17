5d68c381075cc7476efa15124f163071bada3a7a
Venkat <prabhuvenkat46@gmail.com>
2024-10-17 15:21:14 +0530

    no message

--- 2. Padding/Padding/Padding/ContentView.swift
+++ 2. Padding/Padding/Padding/ContentView.swift
@@ -7,15 +7,30 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        Text("Padding")
+        
+        // padding round the object
+            .padding()
+        
+        /*
+        // Padding to the leading Edge
+            .padding(.leading, 20)
+        
+        // Padding to the trailing Edge
+            .padding(.trailing, 20)
+        
+        // Padding to the top Edge
+            .padding(.top, 20)
+        
+        // Padding to the bottom Edge
+            .padding(.bottom, 20)
+         */
+        
+            .background(.cyan)
     }
 }
 
