60bb66cf53c76e0609174a2671073e9612e4c2fa
Venkat <prabhuvenkat46@gmail.com>
2024-10-17 15:20:54 +0530

    Padding

---
+++ 2. Padding/Padding/Padding/PaddingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaddingApp.swift
+//  Padding
+//
+//  Created by apple on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PaddingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
