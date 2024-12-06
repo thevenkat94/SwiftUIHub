cc1a53264b5f6e9bc6dad0dab484a2b28e7478a0
Venkat <prabhuvenkat46@gmail.com>
2024-12-06 12:41:53 +0530

    property wrapper _1

--- 7. Prperty Wrapper/State Property Wrapper/State Property Wrapper/ContentView.swift
+++ 7. Prperty Wrapper/State Property Wrapper/State Property Wrapper/ContentView.swift
@@ -8,12 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var backgroundColor = Color.cyan
+    @State var title = "Title 0"
+    @State var count = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+           
+            backgroundColor
+                .edgesIgnoringSafeArea(.all)
+           
+            VStack(spacing: 20) {
+                Text(title)
+                
+                Text("\(count)")
+                
+                HStack(spacing: 50) {
+                    
+                    Button("Button 1") {
+                        backgroundColor = .red
+                        title = "Title 1"
+                        count += 1
+
+                    }
+                    .foregroundColor(.black)
+                    
+                    Button("Button 2") {
+                        backgroundColor = .gray
+                        title = "Title 2"
+                        count -= 1
+
+                    }
+                    .foregroundColor(.black)
+
+                }
+            }
         }
         .padding()
     }
