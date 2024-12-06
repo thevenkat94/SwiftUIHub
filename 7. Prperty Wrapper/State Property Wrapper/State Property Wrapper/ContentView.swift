ad4837e0d3bbb39c19cbb6a2c8141775c065fe51
Venkat <prabhuvenkat46@gmail.com>
2024-12-06 12:41:27 +0530

    Property Wrapper

---
+++ 7. Prperty Wrapper/State Property Wrapper/State Property Wrapper/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  State Property Wrapper
+//
+//  Created by apple on 06/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 7. Prperty Wrapper/State Property Wrapper/State Property Wrapper/State_Property_WrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_Property_WrapperApp.swift
+//  State Property Wrapper
+//
+//  Created by apple on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct State_Property_WrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
